b113dae8346a3fd2451ad81297919cab31998002
ianmacdonnacha <91433149+IanMacDonnacha@users.noreply.github.com>
2025-08-18 22:41:49 +0100

    DB setup with mongoose & mongoDB Atlas

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 // Create var express that holds express framework
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const mongoDBConn = require('./db');
 const port = 3000;
+
+mongoDBConn(); // Call function to connect to db
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).send('You are connected to the server today!');
