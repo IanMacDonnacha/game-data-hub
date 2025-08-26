017db8ada11df12aa345fccfb5e2b8acc5e1ba57
ianmacdonnacha <91433149+IanMacDonnacha@users.noreply.github.com>
2025-08-26 21:22:53 +0100

    Password hashing hook added to userSchema

--- backend/schema/userSchema.js
+++ backend/schema/userSchema.js
@@ -1,4 +1,14 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
 
 const userSchema = new mongoose.Schema(
   {
