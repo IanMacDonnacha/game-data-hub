f46a2dc7e497621c61edfd38c7e9e84f1617f79a
ianmacdonnacha <91433149+IanMacDonnacha@users.noreply.github.com>
2025-08-28 21:34:33 +0100

    Registration route added to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,37 @@
 // Create var express that holds express framework
 const express = require('express');
-const app = express();
-const cors = require('cors');
 const mongoDBConn = require('./db');
+const cors = require('cors');
+const User = require('./models/User');
+
+const app = express();
 const port = 3000;
 
 mongoDBConn(); // Call function to connect to db
+
+// Middleware
+app.use(express.json()); // Used to parse JSON body
 app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).send('You are connected to the server today!');
+});
+
+app.post('/registration', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
+    const savedUser = newUser.save();
+    res.status(200).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 app.listen(port, () => {
